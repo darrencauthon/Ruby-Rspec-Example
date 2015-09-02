389b9d38f6c76c16cdd5b1c687bd478980d014c3
Darren Cauthon <darren@cauthon.com>
2015-09-02 12:52:30 -0500

    Two-spaces, no tabs, is the Ruby way.

--- spec/apache_restart_spec.rb
+++ spec/apache_restart_spec.rb
@@ -1,11 +1,11 @@
 require '/Users/robertf/Dropbox/Programming/Ruby/rspec_testing/lib/apache_restart'
 
 RSpec.describe Restart do
-	describe 'check' do
-		it 'checks if httpd exists' do
-			@results = false
-			expect(Restart.new.check).to eq(false)
-		end
-	end
-	
+  describe 'check' do
+    it 'checks if httpd exists' do
+      @results = false
+      expect(Restart.new.check).to eq(false)
+    end
+  end
+
 end
