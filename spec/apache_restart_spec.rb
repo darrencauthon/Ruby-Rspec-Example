db0f84b289fe16186bbe104a3f06da7671677541
Darren Cauthon <darren@cauthon.com>
2015-09-02 12:57:08 -0500

    That line was doing nothing.

--- spec/apache_restart_spec.rb
+++ spec/apache_restart_spec.rb
@@ -3,7 +3,6 @@ require_relative 'spec_helper'
 RSpec.describe Restart do
   describe 'check' do
     it 'checks if httpd exists' do
-      @results = false
       expect(Restart.new.check).to eq(false)
     end
   end
